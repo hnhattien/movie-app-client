ed85cce6cf978dce25f785db32ba05d97d77043a
hnhattien <tienit0105@gmail.com>
2023-06-02 11:11:06 +0700

    fix: build

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://MovieSite.org";
+const BASE_URL =
+  "http://ec2-13-239-112-137.ap-southeast-2.compute.amazonaws.com/";
 const config = {
   siteUrl: BASE_URL,
   generateRobotsTxt: true, // (optional)
