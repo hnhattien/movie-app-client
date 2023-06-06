52569fd4d976441ca54a69f9489ab11762923978
hnhattien <tienit0105@gmail.com>
2023-06-06 21:50:27 +0700

    fix: message

--- components/Common/TopBar.js
+++ components/Common/TopBar.js
@@ -6,13 +6,13 @@ export default function TopBar({ content }) {
   // </p>
   return (
     <>
-      <p className="relative bg-slate-900 text-white py-2 px-4 font-semibold">
+      {/* <p className="relative bg-slate-900 text-white py-2 px-4 font-semibold">
         More than 11 thousand movies are collected by me from the Internet
         just within two days.
         <a target="_blank" href="https://github.com/hnhattien">
           hnhattien
         </a>
-      </p>
+      </p> */}
     </>
   );
 }
